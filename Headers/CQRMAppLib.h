c7c77c6339e1553990fc95cb5f864251bd9a1e07
lite <lite@streamax.com>
2023-06-14 15:36:55 +0800

    添加header

---
+++ Headers/CQRMAppLib.h
@@ -0,0 +1,25 @@
+//
+//  CQRMAppLib.h
+//  CQRMAppLib
+//
+//  Created by jr on 2023/6/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CQRMAppLib.
+FOUNDATION_EXPORT double CQRMAppLibVersionNumber;
+
+//! Project version string for CQRMAppLib.
+FOUNDATION_EXPORT const unsigned char CQRMAppLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CQRMAppLib/PublicHeader.h>
+#import <CQRMAppLib/FXNetDevice.h>
+#import <CQRMAppLib/FXNetVideoPlayback.h>
+#import <CQRMAppLib/FXNetRemotePlayback.h>
+#import <CQRMAppLib/NetDeviceInfo.h>
+#import <CQRMAppLib/FXNetStream.h>
+#import <CQRMAppLib/FXNetSearchFile.h>
+#import <CQRMAppLib/FXDefine.h>
+#import <CQRMAppLib/FXFileHeaderInfo.h>
+
