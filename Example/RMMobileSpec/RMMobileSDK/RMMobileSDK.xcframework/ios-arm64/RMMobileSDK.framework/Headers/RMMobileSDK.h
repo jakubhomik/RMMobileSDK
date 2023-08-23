3b10b35c1836327296bc363e239495c3cfc3267d
lite <lite@streamax.com>
2023-08-23 15:31:45 +0800

    增加切换模式方法

---
+++ Example/RMMobileSpec/RMMobileSDK/RMMobileSDK.xcframework/ios-arm64/RMMobileSDK.framework/Headers/RMMobileSDK.h
@@ -0,0 +1,37 @@
+//
+//  RMMobileSDK.h
+//  RMMobileSDK
+//
+//  Created by 刘聪 on 2023/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RMMobileSDK.
+FOUNDATION_EXPORT double RMMobileSDKVersionNumber;
+
+//! Project version string for RMMobileSDK.
+FOUNDATION_EXPORT const unsigned char RMMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMMobileSDK/PublicHeader.h>
+
+
+
+#import <RMMobileSDK/FXFileHeaderInfo.h>
+#import <RMMobileSDK/FXNetVideoPlayback.h>
+#import <RMMobileSDK/FXNetDevice.h>
+#import <RMMobileSDK/FXNetSearchFile.h>
+#import <RMMobileSDK/FXRecordFilelistDateInfo.h>
+#import <RMMobileSDK/FXRecordDateInfo.h>
+#import <RMMobileSDK/FXAudioSampling.h>
+#import <RMMobileSDK/FXDefine.h>
+#import <RMMobileSDK/FXNetServer.h>
+#import <RMMobileSDK/NetServiceInfo.h>
+#import <RMMobileSDK/NetDeviceInfo.h>
+#import <RMMobileSDK/FXNetStream.h>
+#import <RMMobileSDK/FXNetRemotePlayback.h>
+#import <RMMobileSDK/FXManager.h>
+#import <RMMobileSDK/FXLocalPlayback.h>
+#import <RMMobileSDK/RMAudioSession.h>
+#import <RMMobileSDK/UserModel.h>
+
