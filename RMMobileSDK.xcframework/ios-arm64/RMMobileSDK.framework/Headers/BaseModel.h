637e03f5ebb60b376aea3b89737a3684f1487d2b
lite <lite@streamax.com>
2023-08-23 16:41:14 +0800

    增加切换模式方法

---
+++ RMMobileSDK.xcframework/ios-arm64/RMMobileSDK.framework/Headers/BaseModel.h
@@ -0,0 +1,19 @@
+//
+//  BaseModel.h
+//  iOSSDKDemo
+//
+//  Created by Lee on 2018/11/12.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseModel : NSObject
+
+-(instancetype)initWithDictionary:(NSDictionary *)dic;
+
+@end
+
+NS_ASSUME_NONNULL_END
