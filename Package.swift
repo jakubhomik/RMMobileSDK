08271cfbf4c371033cc523722afc5a4d277183f1
Marcin Golli <marcin.golli@ftsgps.com>
2025-01-16 14:02:23 +0100

    letstry

--- Package.swift
+++ Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 let package = Package(
     name: "RMMobileSDK",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
     ],
     products: [
         .library(
             name: "RMMobileSDK",
-            targets: ["RMMobileSDK"]),
+            targets: ["RMMobileSDK"]
+        ),
     ],
     targets: [
         .target(
