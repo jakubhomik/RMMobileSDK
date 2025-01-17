a935a5364c49933838babf8eb7b6087f8c6aa6a8
Jakub Homik <jakub.homik@ftsgps.com>
2025-01-17 12:17:09 +0100

    changed taget in package swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,11 @@ let package = Package(
             targets: ["RMMobileSDK"]),
     ],
     targets: [
-        .target(
-            name: "RMMobileSDK",
-            path: "Sources/RMMobileSDK",
-            publicHeadersPath: "Public"),
+        .binaryTarget(
+                    name: "RMMobileSDK",
+                    url: "https://github.com/jakubhomik/RMMobileSDK/blob/SwiftPackageManager-implementation/Sources/RMMobileSDK.xcframework.zip",
+                    checksum: "349e6247ea805c8cc1d1b04753445ae184ec22b5a749828264156c576e4cc248"
+                ),
         .testTarget(
             name: "RMMobileSDKTests",
             dependencies: ["RMMobileSDK"]
